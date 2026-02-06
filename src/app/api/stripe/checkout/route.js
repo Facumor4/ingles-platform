cb83ebfbcfaa73014bab1f9659c2d993e14d3733
Facundo Mora <facumora3103@gmail.com>
2026-02-06 15:42:39 -0500

    fix: stripe init inside handlers

--- src/app/api/stripe/checkout/route.js
+++ src/app/api/stripe/checkout/route.js
@@ -2,24 +2,21 @@ export const runtime = "nodejs"
 
 import Stripe from "stripe"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+export async function POST(req) {
 
-export async function POST() {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
     line_items: [
       {
-        price: "price_1SxeKjBBvDjH8S8PAuLbr4PS", // tu price real
+        price: "price_1SxeKjBBvDjH8S8PAuLbr4PS",
         quantity: 1,
       },
     ],
-    success_url: "ingles-platform-three.vercel.app",
-    cancel_url: "ingles-platform-three.vercel.app",
-    metadata: {
-      user_id: user.id,
-    },
-
+    success_url: "ingles-platform-rho.vercel.app",
+    cancel_url: "ingles-platform-rho.vercel.app",
   })
 
   return Response.json({ url: session.url })
