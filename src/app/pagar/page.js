0063b9b2d39ee5938bffbeed475b856ceb306515
Facundo Mora <facumora3103@gmail.com>
2026-02-06 16:10:16 -0500

    error despues del boton

--- src/app/pagar/page.js
+++ src/app/pagar/page.js
@@ -2,13 +2,14 @@
 
 import { createClient } from "@supabase/supabase-js"
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
-
 export default function Pagar() {
-  const handlePago = async () => {
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  )
+
+  const pagar = async () => {
     const { data: { session } } = await supabase.auth.getSession()
 
     const res = await fetch("/api/stripe/checkout", {
